84991adce141692d43a6ca08c2dabbb92100ba73
Safoah91 <lampteyanastasia@gmail.com>
2022-11-01 15:42:39 +0000

    setup routes & navitations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Repo from './components/Repo'
+import Footer from './components/Footer'
+import PageNotFound from './components/PageNotFound'
 
 function App() {
   return (
     <>
-      app
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/error-boundary' element={<Repo />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Link to='/'>Repositories</Link>
+      <Link to='/error-boundary'>Error Boundary</Link>
+    </div>
   )
 }
 
